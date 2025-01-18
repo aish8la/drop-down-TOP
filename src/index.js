8d2efd7e9cbd7d78db07671738d08ce32736a62f
aish8la <64914670+aish8la@users.noreply.github.com.>
2025-01-19 01:26:57 +0500

    add function close dropdown if click outside

--- src/index.js
+++ src/index.js
@@ -4,4 +4,12 @@ document.addEventListener("click", e => {
     if (e.target.classList.contains("dropdown-btn")) {
         e.target.closest(".dropdown").classList.toggle("expand");
     }
+
+    const expandedDropdowns = document.querySelectorAll(".expand");
+
+    expandedDropdowns.forEach(element => {
+        if(element !== e.target.closest(".expand")) {
+            element.classList.remove("expand");
+        }
+    });
 });
