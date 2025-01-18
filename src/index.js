2713cc5e32c89c526fea3dc8e4ab305aa80078d8
aish8la <64914670+aish8la@users.noreply.github.com.>
2025-01-19 00:37:36 +0500

    add the code to toggle dropdown

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import "./style.css";
+
+document.addEventListener("click", e => {
+    if (e.target.classList.contains("dropdown-btn")) {
+        e.target.closest(".dropdown").classList.toggle("expand");
+    }
+});
